34be87dc027edb3f101c5142de7fe517d191d69c
Ivan <rawwarior@gmail.com>
2022-06-24 21:33:24 +0300

    lesson 21 done

--- js/script.js
+++ js/script.js
@@ -109,3 +109,77 @@
 // console.log(2 + 2 * 2 != 8);
 // console.log(2 + 2 * 2 !== '6');
 
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('Ok!');
+// }
+
+// (num === 50) ? console.log('Ok!') : console.log('Error');
+
+// const num = 50;
+
+// switch (num) {
+//     case 49:
+//         console.log('Неверно');
+//         break;
+//     case 100:
+//         console.log('Неверно');
+//         break;
+//     case 50:
+//         console.log('В точку!');
+//         break;
+//     default:
+//         console.log('Не в этот раз');
+//         break;
+// }
+
+// const hamburger = 5,
+// fries = 0;
+
+// if (hamburger && fries) {
+//     console.log('Я сыт!');
+// }
+
+// const hamburger = 2,
+// fries = 1;
+
+// if (hamburger === 3 && fries) {
+//     console.log('Мы поели!');
+// } else {
+//     console.log('Мы уходим!');
+// }
+// const hamburger = 2,
+// fries = 1,
+// cola =0,
+// nuggets = 2;
+
+// if (hamburger || cola || fries) {
+//     console.log('Мы поели!');
+// } else {
+//     console.log('Мы уходим!');
+// }
+
+// const hamburger = 2,
+// fries = 1,
+// cola =0,
+// nuggets = 2;
+
+// if (hamburger ===3 && cola ===2 || fries ===3 && nuggets ) {
+//     console.log('Мы поели!');
+// } else {
+//     console.log('Мы уходим!');
+// }
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger === 3 && cola || fries === 3 && nuggets) {
+   console.log(hamburger === 3 && cola || fries === 3 && nuggets);
+}
